162afa2160b5d5d4eb7c8f10b61d897d729d09ee
rochaecp <rochaecp@gmail.com>
2025-04-06 02:31:13 -0300

    imagens catalogo

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -1,6 +1,32 @@
 const catalogo = [
     {
-        "titulo": "O Livro dos Espíritos",
-        "arquivo": "001.jpeg"
+        "titulo": "titulo",
+        "autor": "-",
+        "categoria": "chas",
+		"arquivo": "001.jpg"
     },
+    {
+        "titulo": "titulo",
+        "autor": "-",
+        "categoria": "chas",
+		"arquivo": "002.webp"
+    },	
+    {
+        "titulo": "titulo",
+        "autor": "-",
+        "categoria": "chas",
+		"arquivo": "003.webp"
+    },	
+    {
+        "titulo": "titulo",
+        "autor": "-",
+        "categoria": "chas",
+		"arquivo": "004.webp"
+    },
+    {
+        "titulo": "titulo",
+        "autor": "-",
+        "categoria": "chas",
+		"arquivo": "005.webp"
+    },	
 ];
